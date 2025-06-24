1586d135958e4ba79e2449a9b3c0cc2ee0e19eed
Felix Kemboi <felixkemboi824@gmail.com>
2025-06-24 03:08:15 +0300

    initial commit

---
+++ my-react-app/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1>My React App</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
